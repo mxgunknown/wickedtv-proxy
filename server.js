2015ec3acd44f83692ff0dc7c791c48984889c88
Max <maxgarbett1@gmail.com>
2025-07-09 09:28:01 +0100

    updated code

--- server.js
+++ server.js
@@ -1,36 +1,33 @@
 const express = require("express");
 const request = require("request");
 const cors = require("cors");
-
 const app = express();
+
 app.use(cors());
 
-app.use((req, res) => {
-  const targetUrl = `http://playwithme.pw${req.originalUrl}`;
+app.use("/", (req, res) => {
+  const targetUrl = "http://playwithme.pw" + req.originalUrl;
 
   const headers = {
-    ...req.headers,
-    "host": "playwithme.pw",
-    "origin": "http://playwithme.pw",
-    "referer": "http://playwithme.pw",
-    "user-agent": "Lavf/58.45.100"
+    "User-Agent":
+      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36",
+    "Referer": "http://playwithme.pw/",
+    "Origin": "http://playwithme.pw",
+    "Host": "playwithme.pw",
+    "Accept": "*/*",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Accept-Language": "en-US,en;q=0.9",
   };
 
-  request({
-    url: targetUrl,
-    headers,
-  })
-    .on("response", proxyRes => {
+  req.pipe(
+    request({
+      url: targetUrl,
+      method: req.method,
+      headers,
+    }).on("response", function (response) {
       res.setHeader("Access-Control-Allow-Origin", "*");
-      if (proxyRes.headers["content-type"]) {
-        res.setHeader("Content-Type", proxyRes.headers["content-type"]);
-      }
     })
-    .on("error", err => {
-      console.error("Proxy request error:", err);
-      res.status(500).send("Proxy error");
-    })
-    .pipe(res);
+  ).pipe(res);
 });
 
 const port = process.env.PORT || 3000;
